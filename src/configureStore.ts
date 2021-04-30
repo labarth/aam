b3d19ca036058d636ca6f5b3760e8147da809ddc
alexander matiugin <alexander.matiugin@pandadoc.com>
2021-04-30 16:25:55 +0300

    create store

---
+++ src/configureStore.ts
@@ -0,0 +1,27 @@
+import { combineReducers } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { applyMiddleware, createStore as _createStore, compose } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from 'redux-logger';
+
+export const rootReducer = combineReducers({
+  App: () => 'Hello world!',
+});
+
+
+const sagaMiddleware = createSagaMiddleware();
+
+const createStore = () => {
+  let enhancer = null;
+  if (process.env.NODE_ENV === 'development') {
+    enhancer = composeWithDevTools(applyMiddleware(sagaMiddleware, logger));
+  } else {
+    enhancer = compose(applyMiddleware(sagaMiddleware));
+  }
+
+  const store = _createStore(rootReducer, enhancer);
+
+  return store;
+};
+
+export const store = createStore();
